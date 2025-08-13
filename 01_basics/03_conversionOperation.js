d932ff10a1e08da4b7ba9ca6c91573b63b9d38fa
Sairaj <sairajt@unfyd.com>
2025-08-13 19:56:39 +0530

    learn datatypes and conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "abc";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ad" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "abc";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "afs" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
