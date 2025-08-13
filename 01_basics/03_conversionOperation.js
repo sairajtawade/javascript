6075e49f7a637f7160879627aad673e68a5a83a0
Sairaj <sairajt@unfyd.com>
2025-08-13 20:25:59 +0530

    study prefix and postfix in operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,30 @@ let stringNumber = String(someNumber);
 
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+// **********************************Operations**************************************
+
+// prefix and postfix
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+/*
+If used postfix, with operator after operand (for example, x++),
+ the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x),
+ the increment operator increments and returns the value after incrementing.
+
+*/
